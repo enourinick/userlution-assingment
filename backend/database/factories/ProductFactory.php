b5de15f5f3d16a4b6e0cfbf6b8a332a87398744c
Mohammad Nourinik <m.nourinik@Mohammads-MBP.fritz.box>
2023-09-21 17:37:04 +0200

    add categories and products factories to create fake categories and products

---
+++ backend/database/factories/ProductFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->unique()->word,
+            'image_url' => fake()->imageUrl(600, 600),
+        ];
+    }
+}
