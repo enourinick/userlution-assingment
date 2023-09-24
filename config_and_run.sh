3c184beead4917f6bf297cbe3e5f0d4297f7c938
Mohammad Nourinik <enournick@gmail.com>
2023-09-24 20:19:57 +0200

    add final steps to run the application

---
+++ config_and_run.sh
@@ -0,0 +1,14 @@
+sh!
+ENV=./backend/.env
+ENV_DEV=./backend/.env.dev
+
+if [ -f "$ENV" ]; then
+    echo "$ENV exists."
+    docker-compose up -d
+else 
+    echo "$ENV does not exist."
+    cp "$ENV_DEV" "$ENV"
+    docker-compose up -d --build
+    docker-compose exec -it backend composer install
+    docker-compose exec -it db:seed
+fi
